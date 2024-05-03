c829e3bf4b8bc93a9c02e274aa513073461f9814
Ag <agata.a.rajska@gmail.com>
2024-05-03 14:55:40 +0200

    maybe this will work first time?

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import socket
+import threading
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client, _ = server_socket.accept() # wait for client
-    data = client.recv(4096).decode()
     
+    while True:
+        client, _ = server_socket.accept() # wait for client
+        Thread(target=handle_client, args=[client_socket]).start()
+
+def handle_client(client: socket.socket):
+    data = client.recv(4096).decode()
     if data.split(" ")[1] == "/":
         client.send(b"HTTP/1.1 200 OK\r\n\r\n")
    
@@ -19,8 +24,6 @@ def main():
         
     elif data.split(" ")[1].startswith("/user-agent"):
         text = data.split("\r\n")[2].split(" ")[1]
-        print(text)
-        
         client.send(b"HTTP/1.1 200 OK\r\n")
         client.send(b"Content-Type: text/plain\r\n")
         client.send(f"Content-Length: {len(text)}\r\n".encode("ascii"))
