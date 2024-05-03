70124a3374fd37afee6f1fac2c473a4ee3fc6f36
Ag <agata.a.rajska@gmail.com>
2024-05-03 14:16:19 +0200

    asdkfjh

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client, _ = server_socket.accept() # wait for client
+    data = client.recv(1024)
+    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
