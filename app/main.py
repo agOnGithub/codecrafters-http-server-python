cac346fb959a71219aa5466953238b9419ff047a
Ag <agata.a.rajska@gmail.com>
2024-05-03 18:04:01 +0200

    here we go again

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def handle_client(client: socket.socket, addr, fdir):
         client.send(b"HTTP/1.1 200 OK\r\n\r\n")
         
     elif data.split(" ")[1].startswith("/files/"):
-        fname = path.removeprefix(b"/files/").decode()
+        fname = data.removeprefix(b"/files/").decode()
         fpath = fdir + "/" + fname
         try:
             f = open(fpath, "rb")
