968ebda582314041b68783357dcd90ba09efc660
Ag <agata.a.rajska@gmail.com>
2024-05-03 20:03:25 +0200

    forgot an import

--- app/main.py
+++ app/main.py
@@ -22,24 +22,24 @@ def handle_client(client: socket.socket, addr, fdir):
     elif data.split(" ")[1].startswith("/files/"):
         fname = data.split(" ")[1].split("/")[2]
         fpath = fdir + fname
-        print(data.split(" ")[0])
-        #if data.startswith("GET"):
-        try:
-            f = open(fpath, "rb")
-            blob = f.read()
-            f.close()
-            response = bytearray(b"HTTP/1.1 200 OK\r\n")
-            response.extend(b"Content-Type: application/octet-stream\r\n")
-            response.extend(f"Content-Length: {len(blob)}\r\n\r\n".encode())
-            response.extend(blob)
-            client.send(response)
-        except:
-            client.send(b"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n") 
-       # else:
-           # file_contents = data.split("\r\n\r\n")[1].encode("ascii")
-           # with open(fpath, "wb") as file:
-           #     file.write(file_contents)
-       #client.send(b"HTTP/1.1 201 Created\r\n")
+        #print(data.split(" ")[0])
+        if data.split(" ")[0] == "GET":
+            try:
+                f = open(fpath, "rb")
+                blob = f.read()
+                f.close()
+                response = bytearray(b"HTTP/1.1 200 OK\r\n")
+                response.extend(b"Content-Type: application/octet-stream\r\n")
+                response.extend(f"Content-Length: {len(blob)}\r\n\r\n".encode())
+                response.extend(blob)
+                client.send(response)
+            except:
+                client.send(b"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n") 
+       else:
+            file_contents = data.split("\r\n\r\n")[1].encode("ascii")
+            with open(fpath, "wb") as file:
+               file.write(file_contents)
+            client.send(b"HTTP/1.1 201 Created\r\n")
    
     elif data.split(" ")[1].startswith("/echo/"):
         text = data.split(" ")[1].split("echo")[1].split("/")[1]
