0b304a77ba46e0daec70edf38bd94d787f5cb812
Ag <agata.a.rajska@gmail.com>
2024-05-03 14:34:00 +0200

    dujfhasiluy

--- app/main.py
+++ app/main.py
@@ -17,6 +17,14 @@ def main():
         client.send(b"\r\n")
         client.send(text.encode("ascii"))
         
+    elif data.split(" ")[1].startswith("/user-agent"):
+        text = data.split(" ")[5].split(" ")[1]
+        client.send(b"HTTP/1.1 200 OK\r\n")
+        client.send(b"Content-Type: text/plain\r\n")
+        client.send(f"Content-Length: {len(text)}\r\n".encode("ascii"))
+        client.send(b"\r\n")
+        client.send(text.encode("ascii"))
+        
     else:
         client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 if __name__ == "__main__":
