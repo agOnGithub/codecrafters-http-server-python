bf5e0d09800d7e1df38bb91512c713680509f829
Ag <agata.a.rajska@gmail.com>
2024-05-03 14:23:40 +0200

    dujfhasiluy

--- app/main.py
+++ app/main.py
@@ -4,7 +4,11 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, _ = server_socket.accept() # wait for client
     data = client.recv(1024)
-    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    
+    if data.split(" ")[1] == "/":
+        client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
