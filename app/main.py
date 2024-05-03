f7b04805e5e2e045fa4716404f5008eb4163e1f2
Ag <agata.a.rajska@gmail.com>
2024-05-03 14:25:00 +0200

    ashgdfliuawhefi

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, _ = server_socket.accept() # wait for client
-    data = client.recv(1024)
+    data = client.recv(1024).decode()
     
     if data.split(" ")[1] == "/":
         client.send(b"HTTP/1.1 200 OK\r\n\r\n")
